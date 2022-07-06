2ba6420b798b80277ab4cbda0cfcb88731e11861
sergiioss <caos13es@hotmail.com>
2022-07-06 17:15:07 +0200

    Se utilizao el token en la llamada a axios

--- src/Containers/Admin/Admin.jsx
+++ src/Containers/Admin/Admin.jsx
@@ -1,35 +1,50 @@
 import React, {useState, useEffect} from "react"
 import "./Admin.css"
 import axios from "axios"
-
+import {useDispatch, useSelector} from 'react-redux'
 import { Button } from "react-bootstrap"
+import { loginUsuario, selectDatosUsuario } from "../User/userSlice"
 
 
-const Admin = (props) => {
+const Admin =  (props) => {
 
-    let [cartelera, setCartelera] = useState([
-
-    ])
-
-     useEffect(() => {
-         axios.get('https://videoclub-backend.herokuapp.com/alquileres/')
-         .then(resp => {
+    const token = useSelector(selectDatosUsuario);
+    
+    
+    console.log(token.token)
+    const mostrarAlquileres = async () =>{
+        try{
+        const config = {
+            headers: {
+                "Authorization":`Bearer ${token.token}`
+            }
+        }
+        axios.get('https://videoclub-backend.herokuapp.com/alquileres',config)
+        .then(resp =>{
             console.log(resp)
-            setCartelera(
-               resp.data
-            )
         })
+    } catch (error){
+        console.log(error)
+    }}
+
+    useEffect(()=>{
+        
+    },[])
+    
+     useEffect(() => {
+
     },[])
      
 
     return (
         <div className="admin">
-            <Button className="adminb" variant="outline-success" >Alquileres</Button>
+            <Button className="adminb" variant="outline-success" onClick={()=> mostrarAlquileres()} >Alquileres</Button>
             <Button className="adminb"  variant="outline-success">Usuarios</Button>
             
 
         </div>
     )
 }
+
 
 export default Admin
